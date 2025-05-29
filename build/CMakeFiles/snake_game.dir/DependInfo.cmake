063696b81f752fc04717cfdb443fd7c5a56e6bb1
sammykeng <nnquocanh050612@gmail.com>
2025-05-29 15:48:12 +0700

    fix record, featured, bugs

---
+++ build/CMakeFiles/snake_game.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/thutrang/Desktop/snake/snakegame/game.cpp" "CMakeFiles/snake_game.dir/snakegame/game.cpp.o" "gcc" "CMakeFiles/snake_game.dir/snakegame/game.cpp.o.d"
+  "/Users/thutrang/Desktop/snake/snakegame/main.cpp" "CMakeFiles/snake_game.dir/snakegame/main.cpp.o" "gcc" "CMakeFiles/snake_game.dir/snakegame/main.cpp.o.d"
+  "/Users/thutrang/Desktop/snake/snakegame/menu.cpp" "CMakeFiles/snake_game.dir/snakegame/menu.cpp.o" "gcc" "CMakeFiles/snake_game.dir/snakegame/menu.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/snake_game.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snake_game.dir/snakegame/game.cpp.o"
+  "CMakeFiles/snake_game.dir/snakegame/game.cpp.o.d"
+  "CMakeFiles/snake_game.dir/snakegame/main.cpp.o"
+  "CMakeFiles/snake_game.dir/snakegame/main.cpp.o.d"
+  "CMakeFiles/snake_game.dir/snakegame/menu.cpp.o"
+  "CMakeFiles/snake_game.dir/snakegame/menu.cpp.o.d"
+  "snake_game"
+  "snake_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snake_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
